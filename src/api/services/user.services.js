feb5d43bdd56748135d3bfe5108409b863b905ca
juraantoniv <juraantoniv@gmail.com>
2023-02-04 19:10:49 +0200

    ok

--- src/api/services/user.services.js
+++ src/api/services/user.services.js
@@ -2,5 +2,6 @@ import {axiosRequest} from "../../axios.service/axios.service";
 import {requestsLinks} from "../urls";
 
 export const userRequest = {
+
     getAll:()=>axiosRequest.get(requestsLinks.users)
 }
