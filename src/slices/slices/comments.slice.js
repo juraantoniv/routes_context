60c373aa845ca4a905d783b34016a412a14504d4
juraantoniv <juraantoniv@gmail.com>
2023-02-07 17:07:20 +0200

    ok

--- src/slices/slices/comments.slice.js
+++ src/slices/slices/comments.slice.js
@@ -10,7 +10,7 @@ const initialState ={
 }
 
 
-const getAllAsyncPosts = createAsyncThunk(
+const getAllAsyncComments = createAsyncThunk(
     'userSlice/getAll',
     async (_,{rejectWithValue})=>{
 
